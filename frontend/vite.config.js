fc822d1ec84560fa1f6bccc763746197ced064dd
Adbul Samad <samadpakhtoon09@gmail.com>
2025-01-19 17:00:32 +0500

    change viteconfig and rebuild frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Adjust this if deploying to a subdirectory
   build: {
-    outDir: "dist", // Default is 'dist', make sure it matches
+    outDir: "dist", // Make sure this matches the folder name
   },
 });
