d8c9d622675d1baef8ac06a2b5833c0e4d8fe383
Adbul Samad <samadpakhtoon09@gmail.com>
2025-01-19 16:28:27 +0500

    change name of frontend/src/main to Main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,7 @@
 import React, { useEffect, useState } from "react";
-import Main from "./Components/mainComponent";
-// import AnimatedIntro from "./Components/Pages/AnimatedIntro";
+import Main from "./Components/Main";
 
 const App = () => {
-  // const [showIntro, setShowIntro] = useState(true);
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setShowIntro(false);
-  //   }, 3000);
-  //   return () => clearTimeout(timer);
-  // }, []);
-  // return <>{showIntro ? <AnimatedIntro /> : <Main />}</>;
   return <Main />;
 };
 
