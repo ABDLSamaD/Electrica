c96e04d1e099b48a7dc0398f219e6f6d10fe8e3a
Adbul Samad <samadpakhtoon09@gmail.com>
2025-01-19 15:29:30 +0500

    changes for deploy code root to backend

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const { rateLimit } = require("express-rate-limit");
 const helmet = require("helmet");
-const path = require("path");
+// const path = require("path");
 const routes = require("./src/routes/routes");
 const adminRoutes = require("./src/routes/route-admin");
 const connectionDatabase = require("./src/models/connection");
@@ -45,7 +45,7 @@ app.use(cookieParser());
 app.use(bodyparser.json());
 
 // frontend path resolve config
-app.use(express.static(path.join(_dirname, "/frontend/dist")));
+// app.use(express.static(path.join(_dirname, "/frontend/dist")));
 
 // express-session
 // const isProduction = process.env.NODE_ENV === "production";
@@ -68,9 +68,9 @@ app.use(session(sessionConfig));
 // routes call
 app.use("/api/auth", routes);
 app.use("/api/adminauth", adminRoutes);
-app.get("*", (_, res) => {
-  res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
-});
+// app.get("*", (_, res) => {
+//   res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+// });
 
 // contact support
 app.post("/user/contact-support", async (req, res) => {
