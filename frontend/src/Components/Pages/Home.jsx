25e5ee2fb0eabc8012228f1635a9aaa0c210e375
Abdul Samad <142105107+ABDLSamaD@users.noreply.github.com>
2025-01-25 14:09:20 +0500

    Update Home.jsx
    fix useState is not defined in home

--- frontend/src/Components/Pages/Home.jsx
+++ frontend/src/Components/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import HeaderMain from "./HeaderMain";
 import { FaRegSmileBeam, FaCheckCircle, FaClock } from "react-icons/fa";
 import { motion } from "framer-motion";
