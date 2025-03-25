2ce8a4e8a565ca0dad9bbdb6510cc26a955237a6
Your Name <samadpakhtoon09@gmail.com>
2025-03-25 17:58:41 +0500

    fix changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
+  import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  uild: { manifest: true, outDir: "./dist" },
+  base: "/",
+  root: "./src",
   plugins: [react()],
-  build: {
-    outDir: "dist", // This ensures the build output is in the 'dist' folder
-  },
 });
