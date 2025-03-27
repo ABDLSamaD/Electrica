165bc15db727a9e5c76073c033f700999850e01f
Your Name <samadpakhtoon09@gmail.com>
2025-03-27 22:20:17 +0500

    work in progress

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: ".", // Ensure Vite uses the correct root
   build: {
     outDir: "dist", // Ensure output is in 'dist/'
     emptyOutDir: true, // Clears old builds
-    assetsDir: "assets",
   },
   plugins: [react()],
   base: "./",
