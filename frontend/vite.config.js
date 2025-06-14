163786f389e3a54b9432e060424931486c4f1a33
Adbul Samad <absamadkhan91@gmail.com>
2025-06-14 17:19:21 +0500

    fix erros w/i/p

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/", // ✅ Ensure correct base path for assets
   build: {
     outDir: "dist", // Ensure output is in 'dist/'
     emptyOutDir: true, // Clears old builds
     assetsDir: "assets", // Ensure assets go inside /assets/
   },
   plugins: [react()],
-  base: "/", // ✅ Ensure correct base path for assets
 });
