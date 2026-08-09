a38bc9f088af75eb88308de83b55159c8bb8b271
Abdul Samad <142105107+ABDLSamaD@users.noreply.github.com>
2026-08-09 09:55:05 +0000

    feat: add UserProjectReview component and index file for asset management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,16 @@ import "./styles.css";
 import "./axiosSetup";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/">
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter basename="/">
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
